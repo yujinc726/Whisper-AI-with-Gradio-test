e63f58acf69f0f8836762334d7cf4efd2a6fb3f5
yujinc726 <yujinc726@gmail.com>
2025-05-04 11:04:14 +0900

    Update whisper_model.py

--- whisper_model.py
+++ whisper_model.py
@@ -10,12 +10,12 @@ current_stable_ts = None
 def load_model(model_size, stable_ts):
     """Load or reload the Whisper model based on the selected model size."""
     global current_model, current_model_size, current_stable_ts
-    # if current_model is not None and not(current_model_size == model_size and current_stable_ts == stable_ts):
-    #     # Clear the existing model from memory
-    #     del current_model
-    #     torch.cuda.empty_cache()  # Clear GPU memory if applicable
-    #if current_model is None:
-    print(f"Loading Whisper model: {model_size} {'(stable-ts)' if stable_ts else ''}")
+    if current_model is not None and not(current_model_size == model_size and current_stable_ts == stable_ts):
+         # Clear the existing model from memory
+        current_model = None
+        torch.cuda.empty_cache()  # Clear GPU memory if applicable
+    if current_model is None:
+        print(f"Loading Whisper model: {model_size} {'(stable-ts)' if stable_ts else ''}")
     if stable_ts:
         current_model = stable_whisper.load_model(model_size)
     else:
